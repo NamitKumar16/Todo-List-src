fdbf28118a85410b1064c4d80bdfd72a85ce6e32
Namit Kumar <69860507+NamitKumar16@users.noreply.github.com>
2022-01-13 14:09:06 +0530

    Component
    Added a new Component for todos and checked that it is working

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,37 @@
+import { Todo } from './../../Todo';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+todos:Todo[]
+  constructor() {
+    this.todos = [
+      {
+        sno:1,
+        title:"This is title1",
+        desc:"Description1",
+        active:true
+      },
+      {
+        sno:2,
+        title:"This is title2",
+        desc:"Description2",
+        active:true
+      },
+      {
+        sno:3,
+        title:"This is title3",
+        desc:"Description3",
+        active:true
+      }
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+}
