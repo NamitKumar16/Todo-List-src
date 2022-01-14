4e78b7d74afa5f39136d72cc5db3398c7d4eeee3
Namit Kumar <69860507+NamitKumar16@users.noreply.github.com>
2022-01-15 01:50:09 +0530

    Basic Todo Functions Working

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -9,13 +9,20 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class TodoItemComponent implements OnInit {
   @Input()
   todo!: Todo;
+  @Input()
+  i!:number
   @Output()
   todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output()
+  todoCheckbox: EventEmitter<Todo> = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {}
   onClick(todo: Todo) {
     this.todoDelete.emit(todo);
     console.log('On click has been triggered');
+  }
+  onCheckboxClick(todo:Todo){
+this.todoCheckbox.emit(todo)
   }
 }
--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
   todos: Todo[];
+  localItem: string | null;
   constructor() {
-    this.todos = [
-      
-    ];
+    this.localItem = localStorage.getItem('todos');
+    if (this.localItem == null) {
+      this.todos = [];
+    } else {
+      this.todos = JSON.parse(this.localItem);
+    }
   }
 
   ngOnInit(): void {}
@@ -19,9 +23,16 @@ export class TodosComponent implements OnInit {
     console.log(todo);
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
   addTodo(todo: Todo) {
     console.log(todo);
     this.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+  toggleTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active = !this.todos[index].active
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
